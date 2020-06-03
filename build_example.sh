e1d8048a1aa565197f410734021316572b60d4ca
Markus Kreidl <mkreidl@opentech.at>
2020-06-03 10:23:27 -0300

    Add build script for example c program
    Signed-off-by: Markus Kreidl <mkreidl@opentech.at>

---
+++ build_example.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eux
+gcc \
+  -L "${glibc_install}/lib" \
+  -I "${glibc_install}/include" \
+  -Wl,--rpath="${glibc_install}/lib" \
+  -Wl,--dynamic-linker="${glibc_install}/lib/ld-linux-x86-64.so.2" \
+  -std=c11 \
+  -D_POSIX_C_SOURCE=200809L \
+  -D_XOPEN_SOURCE=700 \
+  -g \
+  -ggdb \
+  -o test_glibc \
+  -v \
+  -O0 \
+  test_glibc.c \
+  -pthread \
+;
+#ldd ./test_glibc.out
+#./test_glibc.out
+
