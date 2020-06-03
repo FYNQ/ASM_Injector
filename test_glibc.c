abf11f0eaebad2d38a283fbc3f80ade9ce99bc42
Markus Kreidl <mkreidl@opentech.at>
2020-06-03 10:24:13 -0300

    Add example c program for build script
    Signed-off-by: Markus Kreidl <mkreidl@opentech.at>

---
+++ test_glibc.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int foo(char *str) { 
+    return 2;
+}
+
+int bar() {
+  	char* str = "Hello from bar via ASM\n";
+	long len = strlen(str);
+	int ret = 0;
+
+	__asm__("movq $1, %%rax \n\t"
+		"movq $1, %%rdi \n\t"
+		"movq %1, %%rsi \n\t"
+		"movl %2, %%edx \n\t"
+		"syscall"
+		: "=g"(ret)
+		: "g"(str), "g" (len));
+
+    return 1;
+}
+#include<pthread.h>
+pthread_t tid[2];
+
+void* doSomeThing(void *arg)
+{
+    unsigned long i = 0;
+    pthread_t id = pthread_self();
+
+    if(pthread_equal(id,tid[0]))
+    {
+        printf("\n First thread processing\n");
+    }
+    else
+    {
+        printf("\n Second thread processing\n");
+    }
+
+    for(i=0; i<(0xFFFFFFFF);i++);
+
+    return NULL;
+}
+
+
+int main(int argc, char** argv) {
+    char *str;
+    printf("foo=%d bar=%d\n", foo("hahha"), bar());
+
+    /* Initial memory allocation */
+    str = (char *) malloc(15);
+    strcpy(str, "tutorialspoint");
+    printf("String = %s,  Address = %u\n", str, str);
+
+    /* Reallocating memory */
+    str = (char *) realloc(str, 25);
+    strcat(str, ".com");
+    printf("String = %s,  Address = %u\n", str, str);
+
+
+    int i = 0;
+    int err;
+
+    while(i < 2)
+    {
+        err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
+        if (err != 0)
+            printf("\ncan't create thread :[%s]", strerror(err));
+        else
+            printf("\n Thread created successfully\n");
+
+        i++;
+    }
+
+    sleep(5);
+
+    free(str);
+}
