741d7473652b555986d2c0f64854e6b1ce9c9832
Markus Kreidl <mkreidl@opentech.at>
2020-06-09 10:46:08 -0300

    Clean-up test_plugin.c code
    Signed-off-by: Markus Kreidl <mkreidl@opentech.at>

--- test_plugin.c
+++ test_plugin.c
@@ -1,33 +1,5 @@
 #include <stdio.h>
-
-static int res;
-int resi;
-int res2;
-
-char bufx[] = "this is a test";
-/*!
- * \brief Function which is called upon function exit. The function call is inserted by the compiler.
- * \param this_fn Address of the caller function.
- * \param call_site Return address to the function which called this_fn
- */
-/*
-__attribute__((no_instrument_function))
-void __cyfg_profile_func_enter(void *this_fn, void *call_site) {
-    char* str = "cyg enter\n";
-    long len = strlen(str);
-    int ret = 0;
-    printf("this_fn: %p call_site: %p\n", this_fn, call_site);
-
-    __asm__("movq $1, %%rax \n\t"
-            "movq $1, %%rdi \n\t"
-            "movq %1, %%rsi \n\t"
-            "movl %2, %%edx \n\t"
-            "syscall"
-            : "=g"(ret)
-            : "g"(str), "g" (len));
-}
-
-*/
+#include <unistd.h>
 
 struct fs_callbacks {
     int (*open_file)();
@@ -39,11 +11,7 @@ struct fs_callbacks {
 
 
 int write_ext2 () {
-    printf("\nCALLED EXT2\n");
-    //void * addr1 = __builtin_return_address(1);
-    //printf("\n|addr1 %lp|\n", addr1);
-    //void * addr0 = __builtin_return_address(0);
-    //printf("\n|addr0 %lp|\n", addr0);
+    fprintf(stderr, "\nCALLED EXT2\n");
 }
 
 
@@ -61,38 +29,38 @@ enum STATE{working, idle};
 
 inline int 
 add1(char a, unsigned int b){
-    printf("\nadd1\n");
+    fprintf(stderr, "\nadd1\n");
     return a + b;
 }
 
 int state(enum STATE a, char *k) {
-    printf("\ncalled state\n");
+    fprintf(stderr, "\ncalled state\n");
 }
 
 int add2(float a, int b){
-    printf("\ncalled add2\n");
+    fprintf(stderr, "\ncalled add2\n");
     int c = a + b;
     return c;
 }
 
+void fun_char(char *a) {
+    fprintf(stderr, "Passed: %s", a);
+    return;
+}
+
 int fun_void(void *a){
-    printf("\ncalled fun_void\n");
-    return a;
+    fprintf(stderr, "\ncalled fun_void\n");
+    return 0;
 }
 
 
 int blub2 (int a){
-    //void * addr1 = __builtin_return_address(0);
-    //void * addr2 = __builtin_return_address(1);
-    //printf("\n|addr1 %lp|\n", addr1);
-    //printf("\n|addr2 %lp|\n", addr2);
-    //printf("\ncalled blub2: %d\n", a);
     return 0;
 }
 
 
 int blub(struct info *ptr_to_struct, int b){
-    printf("\nblub\n");
+    fprintf(stderr, "\nblub\n");
     int c = ptr_to_struct->a + b;
     return c;
 }
@@ -107,17 +75,17 @@ pthread_t tid[2];
 void* doSomeThing(void *arg)
 {
     char fun[] = "test me";
-    printf("\n%s\n", fun);
+    fprintf(stderr, "\n%s\n", fun);
     unsigned long i = 0;
     pthread_t id = pthread_self();
 
     if(pthread_equal(id,tid[0]))
     {
-        printf("\n First thread processing\n");
+        fprintf(stderr, "\n First thread processing\n");
     }
     else
     {
-        printf("\n Second thread processing\n");
+        fprintf(stderr, "\n Second thread processing\n");
     }
 
     for(i=0; i<(0xFFFFFFFF);i++);
@@ -127,15 +95,17 @@ void* doSomeThing(void *arg)
 
 int main(int argc, char **argv) {
     char fun[] = "main";
-    printf("\n%s\n", fun);
     struct info l_info;
     enum STATE s;
+    fun_char(fun);
     s =  idle;
     blub2(4);
     //state(s, "ggg");
     struct fs_callbacks *f = &FileSystems [0];
     f->write_bytes = write_ext2;
     f->write_bytes();
+    blub2(4);
+    //dump_fun_info(fun, 4, 0);
 
     struct minfo{
         int a;
@@ -154,28 +124,19 @@ int main(int argc, char **argv) {
     l_info.a = 20;
     l_info.b = 20;
     ldef.b = 20;
-    //x = add2(20,
-    //       30);
-    //printf("\n%d\n", x);
-
-   // for (i = 0; i < 20; i++){
-   //     printf("a + b = %d\n", res);
-   // }
     int err;
     i = 0;
-    /*
     while(i < 2)
     {
         err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
         if (err != 0)
-            printf("\ncan't create thread :[%s]", strerror(err));
+            printf("\ncan't create thread :[%s]");
         else
             printf("\n Thread created successfully\n");
 
         i++;
     }
-    */
-    //sleep(5);
+    sleep(5);
     return 0;
 }
 
